044ebe7fab7444b8fad1d9125d971d03b359930c
Sergio <kali.sfp@gmail.com>
2018-08-28 16:37:53 +0200

    Introducido cambio para generación de matrices en la clase main.
    matriz[] mat=new matriz[matrices];
    		for (i=0;i<matrices;i++) {
    			mat[i]=new matriz(filas,columnas,matrices);
    		}
    
    		for (i=0;i<mat.length;i++) {
    			mat[i].IntroduceDatosMatriz(filas, columnas, matrices);
    		}

--- src/EjercicioObligatorio.java
+++ src/EjercicioObligatorio.java
@@ -7,6 +7,7 @@ public class EjercicioObligatorio {
 		int filas=0;
 		int columnas=0;
 		int matrices=0;
+		int i=0;
 		
 		Scanner sc=new Scanner(System.in);
 		
@@ -20,10 +21,17 @@ public class EjercicioObligatorio {
 		matrices=sc.nextInt();
 		
 		
-		matriz mat=new matriz(filas,columnas,matrices);
+//		matriz mat=new matriz(filas,columnas,matrices);
 		
-		mat.IntroduceDatosMatriz(filas, columnas, matrices);
+		matriz[] mat=new matriz[matrices];
+		for (i=0;i<matrices;i++) {
+			mat[i]=new matriz(filas,columnas,matrices);
+		}
+		
+		for (i=0;i<mat.length;i++) {
+			mat[i].IntroduceDatosMatriz(filas, columnas, matrices);
+		}
+		
 		
 	}
-
 }
